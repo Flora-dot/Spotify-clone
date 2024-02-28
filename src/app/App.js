2b2074f8603ffeb781d325fd6574612aff604857
Flora Nwakpa <floranwakpa@gmail.com>
2024-02-28 06:32:26 +0100

    folder structure

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import '../components/LandingPageHeader.jsx'
+import { LandingPageHeader } from '../components/LandingPageHeader.jsx';
+
+function App() {
+  return (
+    <>
+    <LandingPageHeader/>
+    </>
+  );
+}
+
+export default App;
