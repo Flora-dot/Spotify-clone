f3786b27d94f7d045d703c2a7cf8e6987f2d3595
Flora Nwakpa <floranwakpa@gmail.com>
2024-03-12 08:58:53 +0100

    styled and added responsive design for premium plans section

--- src/components/PremiumPlans/PremiumPlans.jsx
+++ src/components/PremiumPlans/PremiumPlans.jsx
@@ -1,46 +1,73 @@
-import Styles from './style.module.css';
-import CustomButton from '../CustomButton';
-import { DebitCards } from './DebitCards';
+import Styles from "./style.module.css";
+import CustomButton from "../CustomButton";
+import { DebitCards } from "./DebitCards";
+import { plans } from "./data";
+import CheckIcon from "../../assets/icons/check-icon.svg";
 
-export const PremiumPlans = () =>{
-    const premiumPlans = [1,2,3,4];
-
-    const funct = () => {
-        return(
-            <div>
-                    <CustomButton children={'One-time plans available'}/>
-                    <CustomButton />
-                    <h5>Mini</h5>
-                    <p>From ₹7/day</p>
-                    <p>1 account on mobile only</p>
-                    <hr />
-                    <ul>
-                        <li>Group session</li>
-                        <li>Group session</li>
-                        <li>Group session</li>
-                    </ul>
-                    <CustomButton />
-                    <hr />
-                    <a href="#/terms">Terms and conditions applya</a>
-                </div>
-        );
-    }
-
-    return(
-        <section className={Styles['plans-section']}>
-            <h1>Pick your Premium</h1>
-            <p className={Styles['title']}>Listen without limits on your phone, speaker, and other devices.</p>
-            <div className={Styles['card-icons']}>
-                <DebitCards />
-            </div>
-            <div className={Styles['premium-plans']}>
-                {premiumPlans.map(funct)}
-                
-            </div>
-            <div className={Styles['discount-section']}>
-                <h6 className={Styles['discount-title']}>Special discount for eligible students in university</h6>
-                <CustomButton className={Styles['learn-more-btn']}  children={'Learn more'}/>
-            </div>
-        </section>
-    );
-}
+export const PremiumPlans = () => {
+  return (
+    <section className={Styles["plans-section"]}>
+      <h1>Pick your Premium</h1>
+      <p className={Styles["title"]}>
+        Listen without limits on your phone, speaker, and other devices.
+      </p>
+      <div className={Styles["card-icons"]}>
+        <DebitCards />
+      </div>
+      <div className={Styles["premium-plans"]}>
+        {plans.map((plan) => (
+          <div className={Styles["plan-card"]}>
+            {plan.title !== "Mini" && (
+              <CustomButton
+                children={"1 month free"}
+                className={Styles["free-duration-btn"]}
+              />
+            )}
+            <CustomButton
+              children={"One-time plans available"}
+              className={Styles["plans-btn"]}
+            />
+            <h5 className={Styles["plan-title"]}>{plan.title}</h5>
+            <p>{plan.duration}</p>
+            <p>{plan.account}</p>
+            {plan.title === "Mini" ? (
+              <hr className={Styles["hr-mini"]} />
+            ) : (
+              <hr className={Styles["hr"]} />
+            )}
+            <ul>
+              {plan.offers.map(
+                (offer, index) =>
+                  // Render list item only if feature exists
+                  offer && (
+                    <span>
+                      <img src={CheckIcon} alt="" />
+                      <li key={index}>{offer}</li>
+                    </span>
+                  )
+              )}
+            </ul>
+            <CustomButton className={Styles['view-plans-btn']} children={'VIEW PLANS'}/>
+            <hr className={Styles["second-line"]} />
+            <a href="#/terms">
+              Terms and conditions apply.
+              <span>
+                {plan.title !== "Mini" &&
+                  " 1 month free not available for users who have already tried Premium."}
+              </span>
+            </a>
+          </div>
+        ))}
+      </div>
+      <div className={Styles["discount-section"]}>
+        <h6 className={Styles["discount-title"]}>
+          Special discount for eligible students in university
+        </h6>
+        <CustomButton
+          className={Styles["learn-more-btn"]}
+          children={"Learn more"}
+        />
+      </div>
+    </section>
+  );
+};
---
+++ src/components/PremiumPlans/data.js
@@ -0,0 +1,41 @@
+export const plans = [
+    {
+        title:'Mini',
+        duration: 'From ₹7/day',
+        account: '1 account on mobile only',
+        offers: ['Ad-free music listening on mobile', 'Group Session',  'Download 30 songs on 1 mobile device',]
+        // offerA: 'Ad-free music listening on mobile',
+        // offerB: 'Group Session',
+        // offerC: 'Download 30 songs on 1 mobile device',
+    },
+    {
+        title:'Individual',
+        duration: '₹119/month after offer period',
+        account: '1 account',
+        offers: ['Ad-free music listening on mobile', 'Group Session', 'Download 10k songs/device on 5 devices',]
+        // offerA: 'Ad-free music listening on mobile',
+        // offerB: 'Group Session',
+        // offerC: 'Download 10k songs/device on 5 devices',
+    },
+    {
+        title:'Duo',
+        duration: '₹149/month after offer period',
+        account: '2 accounts',
+        offers: ['For couples who live together', 'Ad-free music listening on mobile', 'Group Session', 'Download 10k songs/device on 5 devices per account',]
+        // offerA: 'For couples who live together',
+        // offerB: 'Ad-free music listening on mobile',
+        // offerC: 'Group Session',
+        // offerD: 'Download 10k songs/device on 5 devices per account',
+    },
+    {
+        title:'Family',
+        duration: '₹179/month after offer period',
+        account: 'Up to 6 accounts',
+        offers: ['For family who live together','Block explicit music', 'Ad-free music listening on mobile', 'Group Session', 'Download 10k songs/device on 5 devices',]
+        // offerA: 'For family who live together',
+        // offerB: 'Block explicit music',
+        // offerC: 'Ad-free music listening on mobile',
+        // offerD: 'Group Session',
+        // offerE: 'Download 10k songs/device on 5 devices',
+    }
+];
