4d6653ce5491c63f105d33189c9bf9c0f1e033b8
Flora Nwakpa <floranwakpa@gmail.com>
2024-03-23 09:27:21 +0100

    changed routing

--- src/app/App.js
+++ src/app/App.js
@@ -3,26 +3,39 @@ import './App.css';
 import Landing  from '../pages/Landing';
 import { Login } from './pages/Login';
 import { NotFound } from './pages/NotFound';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-const route = createBrowserRouter([
-  {
-    path: '',
-    element: <Landing />,
-  },
-  {
-    path: '/Login',
-    element: <Login />
-  },
-  {
-    path: '*',
-    element: <NotFound />
-  }
-]);
+// const route = createBrowserRouter([
+//   {
+//     path: '',
+//     element: <Landing />,
+//   },
+//   {
+//     path: '/Login',
+//     element: <Login />
+//   },
+//   {
+//     path: '*',
+//     element: <NotFound />
+//   }
+// ]);
+// function App() {
+//   return (
+//     <RouterProvider router={route}></RouterProvider>
+//   );
+// }
+
 function App() {
-  return (
-    <RouterProvider router={route}></RouterProvider>
+  return(
+    <Router>
+      <Routes>
+        <Route path="" element={<Landing />} />
+        <Route path="/login" element={<Login />} /> {/* Use element prop instead of Component */}
+        <Route path="*" element={<NotFound />} /> {/* Add a route for 404 page */}
+      </Routes>
+    </Router>
   );
 }
 
