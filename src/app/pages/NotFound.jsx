731d28e71d171b7a20ab78abffab69373614acd7
Flora Nwakpa <floranwakpa@gmail.com>
2024-03-24 23:54:38 +0100

    styled 404 page

--- src/app/pages/NotFound.jsx
+++ src/app/pages/NotFound.jsx
@@ -1,5 +1,14 @@
+import CustomButton from "../../components/CustomButton";
+import style from "./Login.module.css"
+
 export function NotFound() {
     return(
-        <h1>404</h1>
+        <section className={style['not-found']}>
+            <div className={style['not-found-content']}>
+                <h1>4<span>0</span>4</h1>
+                <p>The page you are looking for might have been removed, had it's name changed or is temporarily unavailable. </p>
+                <a href="/"><CustomButton children={'HOMEPAGE'}/></a>
+            </div>
+        </section>
     );
 }
