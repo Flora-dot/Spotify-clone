6f7470c3c2927c4c95d297ee4daa6c218c642462
Flora Nwakpa <floranwakpa@gmail.com>
2024-03-27 14:12:27 +0100

    updated routing

--- src/app/App.js
+++ src/app/App.js
@@ -2,48 +2,48 @@ import './App.css';
 import Landing  from './pages/Landing';
 import { Login } from './pages/Login';
 import { NotFound } from './pages/NotFound';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-const route = createBrowserRouter([
-  {
-    path: '',
-    element: <Landing />,
-  },
-  {
-    path: '/Login',
-    element: <Login />
-  },
-  {
-    path: '*',
-    element: <NotFound />
-  }
-]);
-function App() {
-  return (
-    <>
-    <ToastContainer />
-    <RouterProvider router={route}></RouterProvider>
-    </>
-  );
-}
-
+// const route = createBrowserRouter([
+//   {
+//     path: '',
+//     element: <Landing />,
+//   },
+//   {
+//     path: '/Login',
+//     element: <Login />
+//   },
+//   {
+//     path: '*',
+//     element: <NotFound />
+//   }
+// ]);
 // function App() {
-//   return(
+//   return (
 //     <>
 //     <ToastContainer />
-//     <Router>
-//       <Routes>
-//         <Route path="" element={<Landing />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="*" element={<NotFound />} />
-//       </Routes>
-//     </Router>
+//     <RouterProvider router={route}></RouterProvider>
 //     </>
 //   );
 // }
+
+function App() {
+  return(
+    <>
+    <ToastContainer />
+    <Router>
+      <Routes>
+        <Route path="" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+    </>
+  );
+}
 
 export default App;
